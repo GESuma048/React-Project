aca7044cc82392352e0c427f9c5573aff3d7da46
SumaEswaraiah <sumageswaraiah@gmail.com>
2023-09-17 23:18:52 +0530

    SuperSquad react project

---
+++ supersquad/src/components/App.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import CharacterList from './characterlist';
+import HeroList from './herolist';
+import SquadStats from './SquadStats';
+import '../styles/index.css'
+
+class App extends Component{
+    render(){
+        return(
+            <div className="App">
+                <h2>SuperSquad</h2>
+                <div className="col-md-4"><CharacterList/></div>
+                <div className="col-md-4"><HeroList/></div>
+                <div className="col-md-4"><SquadStats/></div>
+            </div>
+        )
+    }
+}
+export default App;
